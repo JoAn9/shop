e67149aa8ae21d6fd16ebe1bd76030e92de66b2b
JoAn <joannacalinska@gmail.com>
2020-02-10 22:40:39 +0100

    refactor nav & add logo

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../img/logo.png';
 
 function Navigation() {
   return (
-    <nav>
-      <h1>
-        <a href="/">Cool Page</a>
-      </h1>
+    <nav className="nav">
+      <Link to="/">
+        <img src={logo} alt="" style={{ width: '60px' }} />
+      </Link>
       <ul>
         <li>
-          <Link to="/login">Login</Link>
+          <Link to="/products" className="link">
+            All products
+          </Link>
         </li>
         <li>
-          <Link to="/products">All products</Link>
+          <Link to="/admin" className="link">
+            Admin
+          </Link>
         </li>
         <li>
-          <Link to="/admin">Admin</Link>
+          <Link to="/register" className="link">
+            Register
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className="link">
+            Login
+          </Link>
         </li>
       </ul>
     </nav>
