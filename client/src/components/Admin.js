f1769dde293652cd842ced7d18d5d6c91ab4873e
JoAn <joannacalinska@gmail.com>
2020-02-15 22:49:52 +0100

    Admin comp

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,14 +1,104 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import axios from 'axios';
+import { LOGIN_SUCCESS } from '../store/types';
+import { AuthContext } from '../App';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 400,
+      display: 'flex',
+      flexDirection: 'column',
+      alignContent: 'center',
+    },
+    '& button': {
+      backgroundColor: 'var(--primary-color)',
+      marginTop: theme.spacing(5),
+    },
+  },
+}));
 
 function Admin() {
-  // @add contex to check if authenticated
+  const { dispatch, state } = useContext(AuthContext);
+  const classes = useStyles();
+
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorLogin, setErrorLogin] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+
+  const handleChangeLogin = event => {
+    setErrorLogin(false);
+    setLogin(event.target.value);
+  };
+
+  const handleChangePassword = event => {
+    setErrorPassword(false);
+    setPassword(event.target.value);
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (!login) {
+      return setErrorLogin(true);
+    } else if (!password) {
+      return setErrorPassword(true);
+    }
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const body = JSON.stringify({ login, password });
+
+    try {
+      const res = await axios.post('/auth/admin', body, config);
+      const {
+        data: { token },
+      } = res;
+      dispatch({ type: LOGIN_SUCCESS, payload: token });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  if (state.isAuthenticated) {
+    return <Redirect to="/admin/products" />;
+  }
+
+  console.log(state);
+
   return (
-    <ul>
-      <li>
-        <Link to="/admin/products">Add new product</Link>
-      </li>
-    </ul>
+    <form className={classes.root} onSubmit={e => onSubmit(e)}>
+      <TextField
+        id="login"
+        label="Login"
+        value={login}
+        onChange={handleChangeLogin}
+        variant="outlined"
+        error={errorLogin}
+        helperText={errorLogin && 'This field is required'}
+      />
+      <TextField
+        id="password"
+        label="Password"
+        value={password}
+        onChange={handleChangePassword}
+        variant="outlined"
+        error={errorPassword}
+        helperText={errorPassword && 'This field is required'}
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Login
+      </Button>
+    </form>
   );
 }
 
