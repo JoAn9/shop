780857abf75498878c328ced492e4470f74d8e88
JoAn <joannacalinska@gmail.com>
2020-02-15 21:24:32 +0100

    post to /users - register new user

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   db:
     'mongodb+srv://admin:bLVo4YiGlrziTBK8@cluster0-msj4i.mongodb.net/test?retryWrites=true&w=majority',
+  jwtSecret: 'somethingsecret',
 };
---
+++ routes/users.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+// POST '/users'
+// @register user
+router.post(
+  '/',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Email must be a valid email'),
+    check('password')
+      .isLength({ min: 5 })
+      .withMessage('Password must be at least 5 chars long'),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user)
+        return res.status(400).json({
+          errors: [{ msg: 'User with this email already exists' }],
+        });
+
+      user = new User({ email, password });
+
+      const hash = await bcrypt.hash(password, 10);
+      user.password = hash;
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.jwtSecret,
+        { expiresIn: 60 * 60 * 24 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+module.exports = router;
