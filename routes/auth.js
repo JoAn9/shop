070e50dd9403ddb012afa114a1835b4e53cd60f8
JoAn <joannacalinska@gmail.com>
2020-02-16 22:31:29 +0100

    login user auth (post)

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+const config = require('../config');
+
 const adLogin = 'admin';
 const adPassword = '123';
-const token = 'hahaha';
+const adToken = 'hahaha';
 
 // @route    POST auth/admin
 // @desc     Login admin
@@ -11,9 +17,55 @@ router.post('/admin', (req, res) => {
 
   const { login, password } = req.body;
   if (login === adLogin && password === adPassword) {
-    res.json({ token });
+    res.json({ adToken });
   } else {
     res.status(401).json({ msg: 'Token not valid' });
+  }
+});
+
+// @route    POST auth/user
+// @desc     Login user
+router.post('/user', async (req, res) => {
+  console.log(req.body);
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).json({ errors: [{ msg: 'No email or password' }] });
+  }
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ errors: [{ msg: 'No access' }] });
+    }
+
+    const passwordIsMatch = await bcrypt.compare(password, user.password);
+    console.log(passwordIsMatch);
+
+    if (!passwordIsMatch) {
+      return res.status(400).json({ errors: [{ msg: 'No access' }] });
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.jwtSecret,
+      { expiresIn: 60 * 60 * 24 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
 });
 
