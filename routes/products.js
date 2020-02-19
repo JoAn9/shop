0d62b3a4207df8d6da9fb8ea445977eaf8071630
JoAn <joannacalinska@gmail.com>
2020-02-19 22:25:25 +0100

    handle search

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/product');
+const Product = require('../models/Product');
 
 // @route    GET /products
 // @desc     Get all products
 // @access   Public
 router.get('/', async (req, res) => {
+  const search = req.query.search;
   try {
-    const products = await Product.find();
+    const products = await Product.find({
+      title: new RegExp(search, 'i'),
+    }).sort({ created: -1 });
+    console.log(products);
     res.json(products);
   } catch (err) {
     console.error(err.message);
@@ -16,3 +20,4 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
+// query = {$or:[{firstName:{$regex: req.body.customerName, $options: 'i'}},{lastName:{$regex: req.body.customerName, $options: 'i'}}]}
