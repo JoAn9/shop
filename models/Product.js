f69954e5386cd0329d1ab128630717d266e7c4c5
JoAn <joannacalinska@gmail.com>
2020-03-16 23:37:10 +0100

    save img on BE

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,8 @@ const productSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   created: { type: Date, default: Date.now },
+  // img: { data: Buffer, contentType: String },
+  productImg: { type: String },
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,64 @@
 const express = require('express');
-const Product = require('../models/Product');
 const router = express.Router();
+const Product = require('../models/Product');
+// const Image = require('../models/Image');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './client/public/images');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    // To accept the file pass `true`
+    cb(null, true);
+  } else {
+    // To reject this file pass `false`
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 4,
+  },
+  fileFilter: fileFilter,
+});
+
+// @route     POST admin/products/img
+// @desc      save img
+
+// router.post('/products/img', upload.single('file'), (req, res, next) => {
+
+//   const imgPath = `${UPLOAD_PATH}/${req.file.filename}`;
+//   const image = new Image();
+//   image.img.data = fs.readFileSync(imgPath);
+//   image.img.contentType = 'image';
+//   image.save(function(err, result) {
+//     if (err) return console.log(err.message);
+//     res.send(result);
+//   });
+// });
 
 // @route    POST admin/products
 // @desc     Add a product
-// TODO! // @access   Private
-router.post('/products', async (req, res) => {
+// @access   Private
+router.post('/products', upload.single('productImg'), async (req, res) => {
+  console.log('file', req.file);
   const { title, description } = req.body;
+  const { path } = req.file;
+
   const newProduct = new Product({
     title,
     description,
+    // img: image,
+    productImg: path,
   });
   const errors = newProduct.validateSync();
 
@@ -23,6 +72,7 @@ router.post('/products', async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
+    // res.status(500).json({ error: err });
   }
 });
 
