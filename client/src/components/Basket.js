2dc695b37471eaa47790036bc90e8b31c240541a
JoAn <joannacalinska@gmail.com>
2020-05-21 21:31:13 +0200

    Basket table

--- client/src/components/Basket.js
+++ client/src/components/Basket.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import axios from 'axios';
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveCircleOutlinedIcon from '@material-ui/icons/RemoveCircleOutlined';
+import AddCircleOutlinedIcon from '@material-ui/icons/AddCircleOutlined';
+import { addQuantity, removeQuantity } from '../actions/basket';
 
-function Basket() {
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function Basket({ basketProducts, addQuantity, removeQuantity }) {
+  const classes = useStyles();
+
   const confirmPurchase = async () => {
     // const config = {
     //   headers: {
@@ -16,12 +36,59 @@ function Basket() {
     //   console.log(err);
     // }
   };
+
   return (
     <div>
-      Basket
+      <h2>Basket</h2>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="basket">
+          <TableHead>
+            <TableRow>
+              <TableCell>Product</TableCell>
+              <TableCell align="right">Quantity</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Total price</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {basketProducts.map(item => (
+              <TableRow key={item._id}>
+                <TableCell component="th" scope="row">
+                  {item.title}
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton
+                    aria-label="remove"
+                    onClick={() => removeQuantity(item)}
+                  >
+                    <RemoveCircleOutlinedIcon />
+                  </IconButton>
+                  {item.quantity}
+                  <IconButton
+                    aria-label="add"
+                    onClick={() => addQuantity(item)}
+                  >
+                    <AddCircleOutlinedIcon />
+                  </IconButton>
+                </TableCell>
+                <TableCell align="right">{item.price}</TableCell>
+                <TableCell align="right">
+                  {(item.quantity * item.price).toFixed(2)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <button onClick={confirmPurchase}>I am buying</button>
     </div>
   );
 }
 
-export default Basket;
+const mapStateToProps = state => ({
+  basketProducts: state.basket.products,
+});
+
+export default connect(mapStateToProps, { addQuantity, removeQuantity })(
+  Basket
+);
