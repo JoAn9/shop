40b4a10d382b0dc6184729d102e64c532db23843
JoAn <joannacalinska@gmail.com>
2020-06-05 23:19:58 +0200

    send products from session to FE

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Cart = require('../models/Cart');
+const Product = require('../models/Product');
 const authToken = require('../middleware/authToken');
 
 // @route    POST /cart
@@ -12,8 +13,32 @@ router.post('/', async (req, res) => {
     const cart = new Cart(req.session.cart);
     cart.add(_id, quantity);
     req.session.cart = cart;
-    console.log('req.session.cart', req.session.cart);
-    return res.status(201).send(req.session.cart);
+    return res.status(201).end();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const sessionProducts = req.session.cart.products;
+    const productIds = sessionProducts.map(item => item.id);
+    const products = await Product.find()
+      .where('_id')
+      .in(productIds);
+    let productsToSend = [];
+    for (item of sessionProducts) {
+      let product = products.find(p => p._id == item.id);
+      product = {
+        _id: product._id,
+        title: product.title,
+        price: product.price,
+        quantity: item.quantity,
+      };
+      productsToSend.push(product);
+    }
+    res.status(200).json(productsToSend);
   } catch (err) {
     console.log(err);
     res.status(500).send('Something went wrong');
